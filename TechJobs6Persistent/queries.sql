94267e6a4d56b66db25edb16ff30b5fbb81cf99d
emilyalbert <FunkCrusade@outlook.com>
2023-12-07 21:26:22 -0600

    completed for real with queiries.sql finished. BOOM

--- TechJobs6Persistent/queries.sql
+++ TechJobs6Persistent/queries.sql
@@ -5,13 +5,21 @@
 --Part 1: List the columns and their data types in the Jobs table.
 -- SELECT column_name,data_type 
 -- FROM information_schema.columns 
--- WHERE table_schema = 'tech-jobs' and table_name = 'Jobs'; 
+-- WHERE table_schema = 'tech-jobs' and table_name = 'Jobs';
+
+-- EmplyerId: INT
+-- Id : INT
+-- Name: LONGTEXT 
 
 --Part 2: Write a query to list the names of the employers in St. Louis City.
 
 -- SELECT Name 
 -- FROM Employers
 -- where Location like	'St. Louis';
+
+-- EmployerId : INT
+--Id : INT
+--Name : LONGTEXT
 
 --Part 3: Write a query to return a list of the names and descriptions of all skills that are attached to jobs in alphabetical order.
     --If a skill does not have a job listed, it should not be included in the results of this query.
